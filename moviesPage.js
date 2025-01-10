6f93c3438a8342a5801000c343bc967675446a12
kikoDevv <kiko.devv@gmail.com>
2025-01-10 12:44:04 +0100

    fetched movie info from IMDB

--- moviesPage.js
+++ moviesPage.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const filterBtn = document.querySelector(".filterBtn");
 	const filterMenu = document.querySelector(".filterMenu");
-    const cardContainer = document.querySelector(".cardContainer");
+	const cardContainer = document.querySelector(".cardContainer");
 	// Event listener for filter menu toggle
 	filterBtn.addEventListener("click", () => {
 		if (filterMenu.classList.value === "filterMenu") {
@@ -36,37 +36,40 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 });
 //------------movie api-----------------
-
-// Fetch trending movies from IMDB API
-// 'x-rapidapi-key': '07807008b3msh7188004c6c5cd67p18ca7bjsnbc847e4ae696',
 fetchData();
 async function fetchData() {
-    const url = 'https://imdb236.p.rapidapi.com/imdb/most-popular-movies';
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': '07807008b3msh7188004c6c5cd67p18ca7bjsnbc847e4ae696',
-            'x-rapidapi-host': 'imdb236.p.rapidapi.com'
-        }
-    };
-    try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        //---------------create movie card from api-----------
-        console.log(data.length);
-        data.forEach(movie => {
-            createMovieCard(movie.primaryImage, movie.title, movie.description);
-        });
-
-    } catch (error) {
-        console.error(error);
-    }
+	const url = "https://imdb236.p.rapidapi.com/imdb/most-popular-movies";
+	const options = {
+		method: "GET",
+		headers: {
+			"x-rapidapi-key": "07807008b3msh7188004c6c5cd67p18ca7bjsnbc847e4ae696",
+			"x-rapidapi-host": "imdb236.p.rapidapi.com",
+		},
+	};
+	try {
+		const response = await fetch(url, options);
+		const data = await response.json();
+		//---------------create movie card from api-----------
+		console.log(data.length);
+		console.log(data);
+		data.forEach((movie) => {
+			createMovieCard(
+				movie.primaryImage,
+				movie.title,
+				movie.averageRating,
+				movie.startYear,
+				movie.runtimeMinutes
+			);
+		});
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 //------------function create movie card----------------
-function createMovieCard(img, name, desc) {
-    const cardContainer = document.querySelector('.cardContainer');
-    const movieCardHTML = `
+function createMovieCard(img, name, rating, year, runtime) {
+	const cardContainer = document.querySelector(".cardContainer");
+	const movieCardHTML = `
         <div class="movieCard">
             <img
                 class="moviesImg"
@@ -74,12 +77,23 @@ function createMovieCard(img, name, desc) {
                 alt="movie image not found" />
             <div class="cardInfo">
                 <h2 class="moviesName">${name}</h2>
-                <p class="moviesDes">
-                    ${desc}
-                </p>
+                <div class="movieInfo">
+                    <div class="rating">
+                        <i class="fa-brands fa-imdb fa-1xl"></i>
+						<p class="ratingNumber">${rating}</p>
+                    </div>
+                    <div class="year">
+                        <i class="fa-regular fa-calendar-plus"></i>
+						<p class="yearNumber">${year}</p>
+                    </div>
+                    <div class="runtime">
+                         <i class="fa-regular fa-hourglass-half"></i>
+						<p class="runtimeNumber">${runtime}M</p>
+                    </div>
+                </div>
                 <button class="buyBtn">Buy ticket</button>
             </div>
         </div>
     `;
-    cardContainer.innerHTML += movieCardHTML;
+	cardContainer.innerHTML += movieCardHTML;
 }
